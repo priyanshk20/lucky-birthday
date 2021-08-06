5517f59f4c9d196360b9a92de9be7bdd814fbbfa
priyanshk20 <khandelwalpriyansh85@gmail.com>
2021-08-06 11:33:54 +0530

    completed m11

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const dob = document.querySelector('#birth');
 const luckyNum = document.querySelector('#lucky-number');
 const btn = document.querySelector('#btn');
+const output = document.querySelector('#output');
+const image = document.querySelector('#image');
 
 
-btn.addEventListener('click', () => {
-    console.log(dob.value);
-    console.log(luckyNum.value);
-    preventDefault();
-})
+const compare = (sum,luckyNum) => {
 
+    if(sum % luckyNum === 0) {
+        output.innerHTML="Your Birthday is Lucky 😁";
+        image.src = "/luck.png";
+        image.style.border = "3px solid green";
+    }
+    else {
+        output.innerHTML="Sorry, It's Not Lucky 🙄";
+        image.src = "/unluck.jpg";
+        image.style.border = "3px solid red";
+    }
+}
 
 const checkLucky = () => {
     const bdate = dob.value;
-    const num = luckyNum.value;
-
+    const sum = calcSum(bdate);
+    compare(sum,luckyNum.value);
 }
 
 const calcSum = (bdate) => {
-    bdate.replaceAll("-","");
+    bdate = bdate.replaceAll("-","");
+    let sum = 0;
+
+    for(let i=0;i<bdate.length;i++) {
+        sum = sum + Number(bdate.charAt(i));
+    }
+    return sum;
 }
+
+// const lucky = () => {
+//     const image = document.createElement('img');
+//     image.src  = '/lucky.jpg';
+//     document.querySelector('.graphic').appendChild(image);
+// }
+
+btn.addEventListener('click', checkLucky);
